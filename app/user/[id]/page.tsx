a3d9c30324b0c417245acd4dc4dd820babaab001
coffeeboi0811 <varunn0811@gmail.com>
2025-07-15 21:50:36 +0530

    ui: profile page done

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,9 +1,19 @@
 import ProfilePageDetails from "@/components/ProfilePageDetails";
+import { ResponsiveMasonryGrid } from "@/components/ResponsiveMasonryGrid";
+import { ImagePostCard } from "@/components/ImagePostCard";
+import { imagePosts } from "@/lib/imagePostsData";
 
 function UserProfilePage() {
     return (
         <div>
             <ProfilePageDetails />
+            <main className="w-full px-4 py-8">
+                <ResponsiveMasonryGrid>
+                    {imagePosts.map((post, i) => (
+                        <ImagePostCard key={i} {...post} index={i} />
+                    ))}
+                </ResponsiveMasonryGrid>
+            </main>
         </div>
     );
 }
