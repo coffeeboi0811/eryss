7b19a60d8cf858a341b704e69fdebc6d46519387
coffeeboi0811 <varunn0811@gmail.com>
2025-07-19 22:29:41 +0530

    cloudinary stufff

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function GET() {
@@ -30,3 +30,5 @@ export async function GET() {
         );
     }
 }
+
+export async function POST(request: NextRequest) {}
